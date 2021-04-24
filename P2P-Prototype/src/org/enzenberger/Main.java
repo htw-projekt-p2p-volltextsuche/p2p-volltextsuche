198dcd7c2dbcc67bcef87b8ea0c0f43a46b3c129
zink-l <leon.enzenberger@protonmail.com>
2021-04-24 20:08:30 +0200

    Add explanation for some cases

--- P2P-Prototype/src/org/enzenberger/Main.java
+++ P2P-Prototype/src/org/enzenberger/Main.java
@@ -3,6 +3,7 @@ package org.enzenberger;
 public class Main {
 
     public static void main(String[] args) {
+        //create Peers with unique ids
         PeerClient peerOne = new PeerClient("peerOne");
         PeerClient peerTwo = new PeerClient("peerTwo");
         PeerClient peerThree = new PeerClient("peerThree");
@@ -12,6 +13,7 @@ public class Main {
         PeerClient peerSeven = new PeerClient("peerSeven");
         PeerClient peerEight = new PeerClient("peerEight");
 
+        //register the different peers with other peers that are already part of the network
         try {
             peerOne.registerPeer(peerTwo);
             peerTwo.registerPeer(peerThree);
@@ -21,6 +23,7 @@ public class Main {
             peerSix.registerPeer(peerSeven);
             peerOne.registerPeer(peerEight);
 
+            //start networking
             peerThree.startNetwork();
         } catch (NetworkActiveException e) {
             e.printStackTrace();
@@ -28,7 +31,10 @@ public class Main {
 
         System.out.println();
 
+
         try {
+            //save some random strings in the network specifying an entrypoint. The String are not
+            //necessarily saved at that location.
             peerOne.safeString("Haus");
             peerTwo.safeString("Apfel");
             peerFour.safeString("Besenstiel");
@@ -37,6 +43,7 @@ public class Main {
             peerSix.safeString("Nonne");
             peerEight.safeString("Waffel");
 
+            //search for Strings and print the resulting String containing metadata and the result.
             System.out.println(peerEight.searchString("Besenstiel"));
             System.out.println(peerSix.searchString("Mama"));
             System.out.println(peerOne.searchString("Apfel"));
--- P2P-Prototype/src/org/enzenberger/PeerClient.java
+++ P2P-Prototype/src/org/enzenberger/PeerClient.java
@@ -5,22 +5,32 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
-public class PeerClient implements Peer, PeerNode{
+public class PeerClient implements Peer, PeerNode {
+    //Key-Value pair of the Numbers of the clients and the clients themselves. In reality there would be ips saved
+    //instead of the actual client.
     private Hashtable<Integer, PeerClient> peers;
+
+    // the stored String designated to this peer only
     private final List<String> storage = new LinkedList<>();
+
+    //the id of the peer
     public final String id;
+
+    //the number assigned by the network
     private Integer number;
+
+    //the status of the network. inactive = peers can be added. active = search and storage can be utilized
     Boolean networkAktiv = false;
 
-    PeerClient(String id){
+    PeerClient(String id) {
         this.id = id;
     }
 
     @Override
     public void safeString(String string) throws NetworkInactiveException {
         if (!networkAktiv) throw new NetworkInactiveException();
-        char firstChar = string.replace(" ", "").toLowerCase().charAt(0);
-        int storageNumber = (int) firstChar % peers.size();
+        //get hash-value of string to find on which peer to store the string
+        int storageNumber = getHash(string);
         peers.get(storageNumber).storeString(string);
     }
 
@@ -32,15 +42,19 @@ public class PeerClient implements Peer, PeerNode{
     @Override
     public String searchString(String string) throws NetworkInactiveException {
         if (!networkAktiv) throw new NetworkInactiveException();
-        char firstChar = string.replace(" ", "").toLowerCase().charAt(0);
-        int storageNumber = (int) firstChar % peers.size();
+        int storageNumber = getHash(string);
         return peers.get(storageNumber).getSearchResult(string);
+    }
+
+    private int getHash(String string) {
+        char firstChar = string.replace(" ", "").toLowerCase().charAt(0);
+        return (int) firstChar % peers.size();
     }
 
     @Override
     public String getSearchResult(String string) throws NetworkInactiveException {
         if (!networkAktiv) throw new NetworkInactiveException();
-        for(String storedString: storage){
+        for (String storedString : storage) {
             if (string.equals(storedString)) return number + id + ": Found String " + storedString;
         }
         return number + id + ": There is none String like this";
@@ -48,19 +62,22 @@ public class PeerClient implements Peer, PeerNode{
 
     @Override
     public void registerPeer(PeerClient peerClient) throws NetworkActiveException {
-        if(networkAktiv)throw new NetworkActiveException();
-        if(this.peers == null) {
+        if (networkAktiv) throw new NetworkActiveException();
+        //the first peer needs to create a hashtable storing itself first.
+        if (this.peers == null) {
             this.peers = new Hashtable<>();
             int number = 0;
             this.peers.put(number, this);
             this.setNumber(number);
         }
-        if(!this.peers.contains(peerClient)) {
-            int newPeerNumber= peers.size();
+        //only if the new peer is not registered jet should it be saved
+        if (!this.peers.contains(peerClient)) {
+            int newPeerNumber = peers.size();
             this.peers.put(newPeerNumber, peerClient);
             peerClient.setNumber(newPeerNumber);
             Set<Integer> peerClients = peers.keySet();
-            for(Integer key: peerClients){
+            //all already registered peers need to be notified about the new peer. The same goes in the other direction
+            for (Integer key : peerClients) {
                 PeerClient addressedClient = peers.get(key);
                 addressedClient.notifyNewPeer(peerClient);
                 peerClient.notifyNewPeer(addressedClient);
@@ -70,9 +87,9 @@ public class PeerClient implements Peer, PeerNode{
 
     @Override
     public void notifyNewPeer(PeerClient peerClient) throws NetworkActiveException {
-        if(networkAktiv)throw new NetworkActiveException();
-        if(this.peers == null) this.peers = new Hashtable<>();
-        if(!this.peers.contains(peerClient)){
+        if (networkAktiv) throw new NetworkActiveException();
+        if (this.peers == null) this.peers = new Hashtable<>();
+        if (!this.peers.contains(peerClient)) {
             this.peers.put(peerClient.getNumber(), peerClient);
         }
     }
@@ -82,23 +99,23 @@ public class PeerClient implements Peer, PeerNode{
         else throw new IllegalArgumentException("Number can only be set once!");
     }
 
-    public int getNumber(){
+    public int getNumber() {
         return this.number;
     }
 
     @Override
     public void startNetwork() throws NetworkActiveException {
-        if(networkAktiv)throw new NetworkActiveException();
+        if (networkAktiv) throw new NetworkActiveException();
         Set<Integer> peerClients = peers.keySet();
-        for (Integer key: peerClients){
+        for (Integer key : peerClients) {
             peers.get(key).setNetworkAktive();
         }
     }
 
     @Override
     public void setNetworkAktive() throws NetworkActiveException {
-        if (networkAktiv)throw new NetworkActiveException();
+        if (networkAktiv) throw new NetworkActiveException();
         networkAktiv = true;
-        System.out.println(number + id +": I am alive");
+        System.out.println(number + id + ": I am alive");
     }
 }
